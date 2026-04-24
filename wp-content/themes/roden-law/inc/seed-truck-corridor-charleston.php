d86301f26fb0b8b89fab5c1470e705fc0519180f
blueskystudio.git <brianhaas@macbookpro.lan>
2026-04-24 14:04:07 -0500

    Debug: file-based logging for Charleston seeder

--- wp-content/themes/roden-law/inc/seed-truck-corridor-charleston.php
+++ wp-content/themes/roden-law/inc/seed-truck-corridor-charleston.php
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 error_reporting( E_ALL );
 ini_set( 'display_errors', 1 );
-WP_CLI::log( 'Charleston seeder starting...' );
+file_put_contents( '/tmp/charleston-seeder.log', "Starting charleston seeder at " . date('Y-m-d H:i:s') . "\n" );
 
 /* ------------------------------------------------------------------
    Author attribution
